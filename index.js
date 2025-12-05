3ae3196bc0d5bb25375e807b3cf66a4408a177e4
Vismay Gamit <42162523+vismaygamit@users.noreply.github.com>
2025-12-05 23:52:24 +0530

    update port, cors and hello world api

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ import aiRoutes from "./src/routes/expeditionAIRoutes.js";
 
 const app = express();
 const corsOptions = {
-    origin: "https://pay.openwatersyndicate.com",
+    origin: ["https://pay.openwatersyndicate.com", "https://31.97.144.251:3001"], // Allow multiple origins
     optionsSuccessStatus: 200 // For legacy browsers
 };
 // Enable CORS for all routes
 app.use(cors(corsOptions));
 
 app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Hello World from OWS Pay Backend!");
+});
+
 app.use("/api/ai", aiRoutes);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3001, () => console.log("Server running on port 3000"));
